9ce7b2e5a092bd216a475647d8aa616e2d084c76
lcmongwe <lcmongwe@gmail.com>
2022-04-08 14:06:26 +0300

    like and dislike button created

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  styleUrls: ['./quote.component.css'],
 })
 export class QuoteComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  quotes: Quote[] = [
+    {
+      author: 'Kenneth Matiba',
+      sender: 'Lucy',
+      quote: 'no one lives twice',
+      votes: 0,
+    },
+    {
+      author: 'Mecry Lee',
+      sender: 'Masika',
+      quote: 'no one lives twice',
+      votes: 0,
+    },
+    {
+      author: 'Abram Nest',
+      sender: 'Mongwe',
+      quote: 'no one lives twice',
+      votes: 0,
+    },
+    {
+      author: 'Isaac Newton',
+      sender: 'Diana',
+      quote: 'no one lives twice',
+      votes: 0,
+    },
+    {
+      author: 'Wangari Maathai',
+      sender: 'Debra',
+      quote: 'no one lives twice',
+      votes: 0,
+    },
+    {
+      author: 'P.O Lumumba',
+      sender: 'Daisy',
+      quote: 'no one lives twice',
+      votes: 0,
+    },
+  ];
+  votescount: number = 0;
+  functionlike() {
+    this.votescount++;
+  }
+  functiondislike() {
+    this.votescount--;
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
